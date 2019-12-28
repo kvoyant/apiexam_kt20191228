f3e2dcf63655664beec35b2be59457b238df6170
kvoyant <kvoyant@gmail.com>
2019-12-28 10:21:05 +0900

    로그인 화면 구성

--- app/src/main/java/com/yhkim/apiexam_kt20191228/MainActivity.kt
+++ app/src/main/java/com/yhkim/apiexam_kt20191228/MainActivity.kt
@@ -3,10 +3,18 @@ package com.yhkim.apiexam_kt20191228
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
 }
