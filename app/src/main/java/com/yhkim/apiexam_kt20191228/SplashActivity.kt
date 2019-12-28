149dd3c31709e9024db39ae1c27bfd89ed55c5ec
kvoyant <kvoyant@gmail.com>
2019-12-28 14:15:31 +0900

    Splash 액티비티 생성 - 토큰 저장 여부 확인 =>이동 화면 결정 - postdelay 활용

---
+++ app/src/main/java/com/yhkim/apiexam_kt20191228/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.yhkim.apiexam_kt20191228
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.yhkim.apiexam_kt20191228.utils.ContextUtil
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        openProperActivity()
+    }
+
+    fun openProperActivity() {
+        Handler().postDelayed({
+            if(ContextUtil.getUserToken(mContext) == "") {
+//            로그인 안한 상태 라면 (로그인 토큰이 저장되지 않았다 => 새로 로그인 해야함 )
+                val intent = Intent(mContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+            else {
+//            저장된 토큰 있음 , 메인화면으로 이동
+                val intent = Intent(mContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 1500)
+    }
+
+}
