c6896a0d788349398347d66d3d387acb3aec6891
kvoyant <kvoyant@gmail.com>
2019-12-28 13:38:42 +0900

    로그인 요청 후 성공/실패 분기 처리

--- app/src/main/java/com/yhkim/apiexam_kt20191228/LoginActivity.kt
+++ app/src/main/java/com/yhkim/apiexam_kt20191228/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.yhkim.apiexam_kt20191228
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.yhkim.apiexam_kt20191228.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +37,30 @@ class LoginActivity : BaseActivity() {
             }
 
 //            ID/PW 가 정상 이어야만 실행 가능
-            Toast.makeText(mContext, "정상 입력이라 로그인 시도해야함.", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(mContext, "정상 입력이라 로그인 시도해야함.", Toast.LENGTH_SHORT).show()
+            ConnectServer.postRequestLogin(mContext, inputId, inputPw, object : ConnectServer.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+//test_user / qlalfqjsgh!
+                    Log.d("서버응답 json", json.toString())
+                    /*
+{"code":200,"message":"로그인 성공","data":{"user":{"id":3,"login_id":"test_user","name":"Tester","phone":"01092924848","memo":"tttt","category":{"id":10,"title":"ㄷㄷㄱ","color":"#FF8EFF72"},"is_admin":true,"start_date":null,"expire_date":"2019-09-30","created_at":"2019-09-07 07:32:52","level":1},"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6MywibG9naW5faWQiOiJ0ZXN0X3VzZXIiLCJwYXNzd29yZCI6IjVlNmUyMzM1ZTMyNzQ1MTA4OTU3MDk5NGZkYmUwMTU2In0.g2NYwLbY2suxgH4sZsRWRCF40leWIzUaozZjwyFORK5QG1mWtaTDxWiWYGzC8-mP2WTPC6yUaIEr5x-SLZh5ww"}}
+{"code":400,"message":"아이디가 존재하지 않습니다."}
+{"code":400,"message":"비밀번호가 올바르지 않습니다."}
+                    */
+
+                    val code = json.getInt("code")
+//                    code : 200 이면 로그인 성공 토스트 , code : 200 이 아니면 로그인 실패 토스트
+
+                    runOnUiThread {
+                        if( code == 200 ) {
+                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
         }
     }
 
--- app/src/main/java/com/yhkim/apiexam_kt20191228/utils/ConnectServer.kt
+++ app/src/main/java/com/yhkim/apiexam_kt20191228/utils/ConnectServer.kt
@@ -25,7 +25,8 @@ class ConnectServer {
 
             var request = Request.Builder().url(url)
 //          .header("X-Http-Token", inputToken) //헤더에 실어 보낼 정보가 있다면
-                .post(formData).build()
+//                    헤더(header)로 담아야 하는 데이터가 있다면 여기에 .header("이름", 값) 으로 추가해줘야함
+                .post(formData).build()// 모두 첨부 완료했으면 build()로 마무리. 완성.
 
             client.newCall(request).enqueue(object : Callback{
                 override fun onFailure(call: Call, e: IOException) {
@@ -35,7 +36,9 @@ class ConnectServer {
                 override fun onResponse(call: Call, response: Response) {
                     val body = response.body()!!.string()
                     val json = JSONObject(body) //string으로 저장된 응답을 JSON양식으로 가공.
+//                    json변수의 내용을 분석해서 상황에 따른 처리를 할 수 있게됨.
 
+//                    JSON 상세 분석 or 결과에 따른 처리를 화면(Activity)로 이관.
                     handler?.onResponse(json) //? handler가 있다면 (널이 아니라면)
                 }
 
