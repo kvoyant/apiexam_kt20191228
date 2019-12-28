6c175ad8e1267e011bc5bd2f02cddc5b86ee71c3
kvoyant <kvoyant@gmail.com>
2019-12-28 16:44:09 +0900

    유저 목록을 뿌리는 어댑처 초안작성 - copy & paste 기존꺼 사용

---
+++ app/src/main/java/com/yhkim/apiexam_kt20191228/adapters/UserAdapter.kt
@@ -0,0 +1,31 @@
+package com.yhkim.apiexam_kt20191228.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.yhkim.apiexam_kt20191228.R
+import com.yhkim.apiexam_kt20191228.datas.User
+
+class UserAdapter(context:Context, resId:Int, list:ArrayList<User>) : ArrayAdapter<User>(context, resId, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.user_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        var data = mList.get(position)
+
+
+        return row
+
+    }
+}
